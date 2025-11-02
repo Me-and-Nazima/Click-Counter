42f683ad6f8e67cd405e54817e28aa056371cba0
nikk04202011-arch <nikk04202011@gmail.com>
2025-11-02 14:08:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const button = document.getElementById("Button");
+const text = document.getElementById("countertext");
+let clicks = 0
+button.addEventListener("click", () => {
+  clicks++;
+  
+  text.textContent = clicks;
+});  
